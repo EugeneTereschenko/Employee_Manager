d1f2e0b32c82bd84e7bb3ddeba13d60d9635d062
Yevhen_Tereshchenko <Yevhen_Tereshchenko@epam.com>
2022-01-03 21:31:57 +0200

    Add back end

---
+++ src/main/java/tech/getmyproject/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,11 @@
+package tech.getmyproject.employeemanager.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.getmyproject.employeemanager.model.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+    Optional<Employee> findEmployeeById(Long id);
+}
